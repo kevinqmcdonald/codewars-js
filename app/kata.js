cd3ef44e1590a7a4ec473844b92e4b9a32933bc8
Kevin McDonald (kevinqmcdonald.eth) <kevinqmcdonald@gmail.com>
2021-12-30 16:08:31 -0500

    update defaults

---
+++ app/kata.js
@@ -0,0 +1,3 @@
+// app/kata.js
+
+// Insert code here
---
+++ test/test.js
@@ -0,0 +1,8 @@
+var kata = require("../app/kata");
+var expect = require("chai").expect;
+
+describe("Tests", () => {
+  it("should pass all tests", () => {
+
+  });
+});
