32a588cf6c88b12e21b8c3babb2de031331c375c
kevinqmcdonald.eth <kevinqmcdonald@gmail.com>
2021-12-30 16:32:36 -0500

    solution

--- app/kata.js
+++ app/kata.js
@@ -1,3 +1,16 @@
 // app/kata.js
 
-// Insert code here
+exports.spinWords = function spinWords(string) {
+  let result = "";
+  const words = string.split(" ")
+  words.forEach(w => {
+    if (w.length >= 5) {
+      result += Array.from(w).reverse().join("")
+    } else {
+      result += w;
+    }
+    result += " ";
+  })
+
+  return result.trim();
+}
--- test/test.js
+++ test/test.js
@@ -1,8 +1,14 @@
-var kata = require("../app/kata");
-var expect = require("chai").expect;
+var spinWords = require("../app/kata").spinWords;
+var assert = require("chai").assert;
 
 describe("Tests", () => {
   it("should pass all tests", () => {
-
+    assert.strictEqual(spinWords("Welcome"), "emocleW");
+    assert.strictEqual(spinWords("Hey fellow warriors"), "Hey wollef sroirraw");
+    assert.strictEqual(spinWords("This is a test"), "This is a test");
+    assert.strictEqual(spinWords("This is another test"), "This is rehtona test");
+    assert.strictEqual(spinWords("You are almost to the last test"), "You are tsomla to the last test");
+    assert.strictEqual(spinWords("Just kidding there is still one more"), "Just gniddik ereht is llits one more");
+    assert.strictEqual(spinWords("Seriously this is the last one"), "ylsuoireS this is the last one");
   });
 });
