80025f535313bbe0643578fe67c976b3da7497b0
kevinqmcdonald.eth <kevinqmcdonald@gmail.com>
2021-12-30 21:11:27 -0500

    solution

--- app/kata.js
+++ app/kata.js
@@ -1,3 +1,11 @@
 // app/kata.js
 
-// Insert code here
+exports.addBinary = function addBinary(a, b) {
+  let sum = a + b;
+  let binary = "";
+  while(sum > 0) {
+    binary = `${sum % 2}`.concat(binary);
+    sum = Math.floor(sum / 2);
+  }
+  return binary;
+}
--- test/test.js
+++ test/test.js
@@ -3,6 +3,9 @@ var expect = require("chai").expect;
 
 describe("Tests", () => {
   it("should pass all tests", () => {
-
+    expect(kata.addBinary(1, 1)).to.equal("10");
+    expect(kata.addBinary(1, 2)).to.equal("11");
+    expect(kata.addBinary(5, 9)).to.equal("1110");
+    expect(kata.addBinary(123, 456)).to.equal("1001000011");
   });
 });
