2dbb1de470c2d9387a944f1977f9cfd1b360a3e0
kevinqmcdonald.eth <kevinqmcdonald@gmail.com>
2021-12-30 21:58:55 -0500

    solution

--- app/kata.js
+++ app/kata.js
@@ -1,3 +1,21 @@
 // app/kata.js
 
-// Insert code here
+exports.longestConsec = (strarr, k) => {
+  if (k <= 0 || k > strarr.length) return ""
+
+  let start = 0;
+  let max = strLen(strarr, 0, k);
+  for (let i = 1; i < strarr.length; ++i) {
+    const len = strLen(strarr, i, k);
+    if (len > max) {
+      max = len;
+      start = i;
+    }
+  }
+
+  return strarr.slice(start, start+k).join("");
+}
+
+function strLen(strarr, i, k) {
+  return strarr.slice(i, i+k).join("").length;
+}
--- test/test.js
+++ test/test.js
@@ -1,8 +1,20 @@
 var kata = require("../app/kata");
 var expect = require("chai").expect;
 
+function testing(actual, expected) {
+  expect(actual).to.equal(expected)
+}
+
 describe("Tests", () => {
   it("should pass all tests", () => {
-
+    testing(kata.longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2), "abigailtheta")
+    testing(kata.longestConsec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1), "oocccffuucccjjjkkkjyyyeehh")
+    testing(kata.longestConsec([], 3), "")
+    testing(kata.longestConsec(["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck")
+    testing(kata.longestConsec(["wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"], 2), "wlwsasphmxxowiaxujylentrklctozmymu")
+    testing(kata.longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2), "")
+    testing(kata.longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3), "ixoyx3452zzzzzzzzzzzz")
+    testing(kata.longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15), "")
+    testing(kata.longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0), "")
   });
 });
