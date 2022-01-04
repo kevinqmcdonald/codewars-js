9bf2794329b22431e12fa8d28c56989593ed2ed7
kevinqmcdonald <kevinqmcdonald@gmail.com>
2022-01-04 16:27:21 -0500

    solution

--- app/kata.js
+++ app/kata.js
@@ -1,3 +1,13 @@
 // app/kata.js
 
-// Insert code here
+exports.domainName = (url) => {
+  let regex = new RegExp("https?:\\/\\/(w{3})*|w{3}\\.");
+  let matches = url.match(regex);
+  while(matches) {
+    url = url.substr(regex.lastIndex + matches[0].length);
+    matches = url.match(regex);
+  }
+
+  regex = new RegExp("[a-zA-Z0-9\\-]+");
+  return url.match(regex)[0];
+}
--- test/test.js
+++ test/test.js
@@ -3,6 +3,10 @@ var expect = require("chai").expect;
 
 describe("Tests", () => {
   it("should pass all tests", () => {
-
+    expect(kata.domainName("http://google.com")).to.equal("google");
+    expect(kata.domainName("http://google.co.jp")).to.equal("google");
+    expect(kata.domainName("www.xakep.ru")).to.equal("xakep");
+    expect(kata.domainName("https://youtube.com")).to.equal("youtube");
+    expect(kata.domainName("http://www.zombie-bites.com")).to.equal("zombie-bites");
   });
 });
