99d2d034aa65cfef0f899a3f73d3b6cdcc0f7543
kevinqmcdonald <kevinqmcdonald@gmail.com>
2022-01-04 16:51:44 -0500

    solution

--- app/kata.js
+++ app/kata.js
@@ -1,3 +1,18 @@
 // app/kata.js
 
-// Insert code here
+exports.angrams = (word, words) => {
+  const res = [];
+  for (const w of words) {
+    if (sortLetters(w) === sortLetters(word)) {
+      res.push(w);
+    }
+  }
+
+  return res;
+}
+
+function sortLetters(str) {
+  const arr = str.split("");
+  const sorted = arr.sort();
+  return sorted.join("");
+}
--- test/test.js
+++ test/test.js
@@ -3,6 +3,8 @@ var expect = require("chai").expect;
 
 describe("Tests", () => {
   it("should pass all tests", () => {
-
+    expect(kata.angrams("abba", ["aabb", "abcd", "bbaa", "dada"])).to.have.members(["aabb", "bbaa"]);
+    expect(kata.angrams("racer", ["crazer", "carer", "racar", "caers", "racer"])).to.have.members(["carer", "racer"]);
+    expect(kata.angrams("laser", ["lazing", "lazy", "lacer"])).to.be.empty;
   });
 });
