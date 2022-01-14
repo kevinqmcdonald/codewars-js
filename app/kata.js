0bb0297e1c8411bae74ee8f87e110d1878568186
kevinqmcdonald.eth <kevinqmcdonald@gmail.com>
2022-01-14 16:46:36 -0500

    solution

--- app/kata.js
+++ app/kata.js
@@ -1,6 +1,20 @@
 // app/kata.js
+class Kata {
+  zero(operator) { return operator ? operator(0) : 0; }
+  one(operator) { return operator ? operator(1) : 1; }
+  two(operator) { return operator ? operator(2) : 2; }
+  three(operator) { return operator ? operator(3) : 3; }
+  four(operator) { return operator ? operator(4) : 4; }
+  five(operator) { return operator ? operator(5) : 5; }
+  six(operator) { return operator ? operator(6) : 6; }
+  seven(operator) { return operator ? operator(7) : 7; }
+  eight(operator) { return operator ? operator(8) : 8; }
+  nine(operator) { return operator ? operator(9) : 9; }
 
-// Insert code here
-module.exports = {
-
+  plus(rhs) { return function (lhs) { return lhs + rhs; } }
+  minus(rhs) { return function (lhs) { return lhs - rhs; } }
+  times(rhs) { return function (lhs) { return lhs * rhs; } }
+  dividedBy(rhs) { return function (lhs) { return Math.floor(lhs / rhs); } }
 }
+
+module.exports = Kata;
--- test/test.js
+++ test/test.js
@@ -1,8 +1,16 @@
-var kata = require("../app/kata");
+const Kata = require("../app/kata");
+const {
+  zero, one, two, three, four, five, six, seven, eight, nine,
+  plus, minus, times, dividedBy
+} = new Kata();
 var expect = require("chai").expect;
 
 describe("Tests", () => {
   it("should pass all tests", () => {
-
+    expect(seven(times(five()))).to.equal(35);
+    expect(four(plus(nine()))).to.equal(13);
+    expect(eight(minus(three()))).to.equal(5);
+    expect(six(dividedBy(two()))).to.equal(3);
+    expect(eight(dividedBy(three()))).to.equal(2);
   });
 });
