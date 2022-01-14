c5b9fc9574c6a3ff28fb4d56281ec75c790ecd9b
kevinqmcdonald.eth <kevinqmcdonald@gmail.com>
2022-01-14 17:11:26 -0500

    solution

--- app/kata.js
+++ app/kata.js
@@ -2,7 +2,9 @@
 
 // Insert code here
 class Kata {
-
+  countBits(n) {
+    return n.toString(2).split("").filter(bit => bit === "1").length;
+  }
 }
 
 module.exports = Kata;
--- test/test.js
+++ test/test.js
@@ -1,8 +1,13 @@
 const Kata = require("../app/kata");
+const { countBits } = new Kata();
 const expect = require("chai").expect;
 
 describe("Tests", () => {
   it("should pass all tests", () => {
-
+    expect(countBits(0)).to.equal(0);
+    expect(countBits(4)).to.equal(1);
+    expect(countBits(7)).to.equal(3);
+    expect(countBits(9)).to.equal(2);
+    expect(countBits(10)).to.equal(2);
   });
 });
