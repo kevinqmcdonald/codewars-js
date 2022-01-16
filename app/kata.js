6c2c0e461d365583dda62d5c698e79918c7ecca6
kevinqmcdonald.eth <kevinqmcdonald@gmail.com>
2022-01-16 14:46:49 -0500

    solution

--- app/kata.js
+++ app/kata.js
@@ -2,7 +2,19 @@
 
 // Insert code here
 class Kata {
+  towerBuilder(nFloors) {
+    const tower = [];
+    if (nFloors === 0) return tower;
 
+    const levelLength = 2 * nFloors - 1;
+    for (let i = 1; i <= nFloors; i++) {
+      const stars = new Array(2 * i - 1).fill("*").join("");
+      const spaces = new Array((levelLength - stars.length) / 2).fill(" ").join("");
+      tower.push([spaces, stars, spaces].join(""));
+    }
+
+    return tower;
+  }
 }
 
 module.exports = Kata;
--- test/test.js
+++ test/test.js
@@ -1,8 +1,11 @@
 const Kata = require("../app/kata");
+const { towerBuilder } = new Kata();
 const expect = require("chai").expect;
 
 describe("Tests", () => {
   it("should pass all tests", () => {
-
+    expect(towerBuilder(1)).to.eql(["*"]);
+    expect(towerBuilder(2)).to.eql([" * ","***"]);
+    expect(towerBuilder(3)).to.eql(["  *  "," *** ","*****"]);
   });
 });
