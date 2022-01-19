cb9452c273e566d5f079debc2053bac3a7c5ebe4
kevinqmcdonald.eth <kevinqmcdonald@gmail.com>
2022-01-18 20:08:10 -0500

    solution

--- app/kata.js
+++ app/kata.js
@@ -1,8 +1,26 @@
 // app/kata.js
-
-// Insert code here
 class Kata {
+  add(a, b) {
+    const aDigits = a.split("");
+    const bDigits = b.split("");
+    let sum = "";
+    let carry = 0;
+    for (let i = 0; i < Math.max(a.length, b.length); i++) {
+      const aDigit = (a.length - 1) - i >= 0 ? Number(aDigits[(a.length - 1) - i]) : 0;
+      const bDigit = (b.length - 1) - i >= 0 ? Number(bDigits[(b.length - 1) - i]) : 0;
+      let digitSum = aDigit + bDigit + carry;
+      if (digitSum > 9) {
+        carry = 1;
+        digitSum -= 10;
+      } else {
+        carry = 0;
+      }
 
+      sum = digitSum.toString() + sum;
+    }
+
+    return carry > 0 ? carry.toString() + sum : sum;
+  }
 }
 
 module.exports = Kata;
--- test/test.js
+++ test/test.js
@@ -1,8 +1,15 @@
 const Kata = require("../app/kata");
+const { add } = new Kata();
 const expect = require("chai").expect;
 
 describe("Tests", () => {
   it("should pass all tests", () => {
-
+    expect(add("1", "1")).to.equal("2");
+    expect(add("123", "456")).to.equal("579");
+    expect(add("888", "222")).to.equal("1110");
+    expect(add("1372", "69")).to.equal("1441");
+    expect(add("12", "456")).to.equal("468");
+    expect(add("101", "100")).to.equal("201");
+    expect(add('63829983432984289347293874', '90938498237058927340892374089')).to.equal("91002328220491911630239667963")
   });
 });
