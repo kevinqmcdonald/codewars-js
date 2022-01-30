862e2674f0a97340c6343db016ab0434ce21f57b
kevinqmcdonald <kevinqmcdonald@gmail.com>
2022-01-29 23:29:26 -0500

    solution

--- app/kata.js
+++ app/kata.js
@@ -1,8 +1,31 @@
 // app/kata.js
 
 // Insert code here
-class Kata {
+class PaginationHelper {
+  constructor(collection, itemsPerPage) {
+    this.collection = collection
+    this.itemsPerPage = itemsPerPage
+  }
 
+  itemCount() {
+    return this.collection.length
+  }
+
+  pageCount() {
+    return Math.ceil(this.itemCount() / this.itemsPerPage)
+  }
+
+  pageItemCount(pageIndex) {
+    if (pageIndex < 0 || pageIndex > this.pageCount() - 1) return -1
+    else if (pageIndex < this.pageCount() - 1) return this.itemsPerPage
+    else return this.itemCount() % this.itemsPerPage
+  }
+
+  pageIndex(itemIndex) {
+    if (itemIndex < 0 || itemIndex >= this.itemCount()) return -1
+    else if ((itemIndex + 1) % this.itemsPerPage === 0) return (itemIndex + 1) / this.itemsPerPage - 1
+    else return Math.floor((itemIndex + 1) / this.itemsPerPage)
+  }
 }
 
-module.exports = Kata;
+module.exports = PaginationHelper;
--- test/test.js
+++ test/test.js
@@ -1,8 +1,17 @@
-const Kata = require("../app/kata");
+const PaginationHelper = require("../app/kata");
 const expect = require("chai").expect;
 
 describe("Tests", () => {
   it("should pass all tests", () => {
-
+    const helper = new PaginationHelper(['a', 'b', 'c', 'd', 'e', 'f'], 4);
+    expect(helper.pageCount()).to.equal(2);
+    expect(helper.itemCount()).to.equal(6);
+    expect(helper.pageItemCount(0)).to.equal(4);
+    expect(helper.pageItemCount(1)).to.equal(2);
+    expect(helper.pageItemCount(2)).to.equal(-1);
+    expect(helper.pageIndex(5)).to.equal(1);
+    expect(helper.pageIndex(3)).to.equal(0);
+    expect(helper.pageIndex(20)).to.equal(-1);
+    expect(helper.pageIndex(-1)).to.equal(-1);
   });
 });
