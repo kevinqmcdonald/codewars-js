054b861c03f2aa984db9c971a85154bff941ca5c
kevinqmcdonald <kevinqmcdonald@gmail.com>
2022-01-31 16:22:40 -0500

    solution

--- app/kata.js
+++ app/kata.js
@@ -1,6 +1,28 @@
 // app/kata.js
+class Word {
+  constructor(word) {
+    this.word = word;
+    this.count = 1;
+  }
+}
 
-// Insert code here
+function topThreeWords(text) {
+  const words = [];
+  text.split(" ").filter(w => w !== "").forEach(w => {
+    const trimmedWord = new Word(w.toLowerCase().replace(/[^a-zA-Z']+|^'$/, ""));
+    if (trimmedWord.word !== "") {
+      let foundWord;
+      if ((foundWord = words.find(word => word.word === trimmedWord.word)) !== undefined) {
+        foundWord.count++;
+      } else {
+        words.push(trimmedWord);
+      }
+    }
+  });
 
+  return words.sort((w1, w2) => w2.count - w1.count)
+    .slice(0, words.length > 2 ? 3 : undefined)
+    .map(w => w.word);
+}
 
-//module.exports = ;
+module.exports = topThreeWords;
--- test/test.js
+++ test/test.js
@@ -1,8 +1,20 @@
-//const { } = require("../app/kata");
+const topThreeWords = require("../app/kata");
 const expect = require("chai").expect;
 
 describe("Tests", () => {
   it("should pass all tests", () => {
-
+    expect(topThreeWords("a a a  b  c c  d d d d  e e e e e")).to.eql(['e','d','a'])
+    expect(topThreeWords("a a c b b")).to.eql(['a','b','c'])
+    expect(topThreeWords("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e")).to.eql(['e','ddd','aa'])
+    expect(topThreeWords("  //wont won't won't ")).to.eql(["won't", "wont"])
+    expect(topThreeWords("  , e   .. ")).to.eql(["e"])
+    expect(topThreeWords("  ...  ")).to.eql([])
+    expect(topThreeWords("  '  ")).to.eql([])
+    expect(topThreeWords(`In a village of La Mancha, the name of which I have no desire to call to
+      mind, there lived not long since one of those gentlemen that keep a lance
+      in the lance-rack, an old buckler, a lean hack, and a greyhound for
+      coursing. An olla of rather more beef than mutton, a salad on most
+      nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
+      on Sundays, made away with three-quarters of his income.`)).to.eql(['a','of','on'])
   });
 });
