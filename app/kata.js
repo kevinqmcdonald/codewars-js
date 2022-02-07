cc47f440f7b4c0ff82c7deba9742be59fcdcda70
kevinqmcdonald.eth <kevinqmcdonald@gmail.com>
2022-02-07 00:30:41 -0500

    solution

--- app/kata.js
+++ app/kata.js
@@ -1,6 +1,19 @@
 // app/kata.js
+function hamming(n) {
+  const h = Array(n).fill(1);
+  let h2 = 2;
+  let h3 = 3;
+  let h5 = 5;
+  let i = 0, j = 0, k = 0;
 
-// Insert code here
+  for (let ix = 1; ix < n; ix++) {
+    h[ix] = Math.min(h2, Math.min(h3, h5));
+    if (h[ix] === h2) h2 = 2 * h[++i];
+    if (h[ix] === h3) h3 = 3 * h[++j];
+    if(h[ix] === h5) h5 = 5 * h[++k];
+  }
 
+  return h[n-1];
+}
 
-//module.exports = ;
+module.exports = hamming;
--- test/test.js
+++ test/test.js
@@ -1,8 +1,26 @@
-//const { } = require("../app/kata");
+const hamming = require("../app/kata");
 const expect = require("chai").expect;
 
 describe("Tests", () => {
   it("should pass all tests", () => {
-
+    expect(hamming(1)).to.equal(1);
+    expect(hamming(2)).to.equal(2);
+    expect(hamming(3)).to.equal(3);
+    expect(hamming(4)).to.equal(4);
+    expect(hamming(5)).to.equal(5);
+    expect(hamming(6)).to.equal(6);
+    expect(hamming(7)).to.equal(8);
+    expect(hamming(8)).to.equal(9);
+    expect(hamming(9)).to.equal(10);
+    expect(hamming(10)).to.equal(12);
+    expect(hamming(11)).to.equal(15);
+    expect(hamming(12)).to.equal(16);
+    expect(hamming(13)).to.equal(18);
+    expect(hamming(14)).to.equal(20);
+    expect(hamming(15)).to.equal(24);
+    expect(hamming(16)).to.equal(25);
+    expect(hamming(17)).to.equal(27);
+    expect(hamming(18)).to.equal(30);
+    expect(hamming(19)).to.equal(32);
   });
 });
