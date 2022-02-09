398af2fc69eac070d5e9ecb547c7d097691ade76
kevinqmcdonald.eth <kevinqmcdonald@gmail.com>
2022-02-09 00:06:00 -0500

    solution

--- app/kata.js
+++ app/kata.js
@@ -1,6 +1,29 @@
 // app/kata.js
+function removeZeros(array) {
+  // Sort "array" so that all elements with the value of zero are moved to the
+  // end of the array, while the other elements maintain order.
+  // [0, 1, 2, 0, 3] --> [1, 2, 3, 0, 0]
+  // Zero elements also maintain order in which they occurred.
+  // [0, "0", 1, 2, 3] --> [1, 2, 3, 0, "0"]
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    // prevent infinite loops
+    if (count === array.length) break;
 
-// Insert code here
+    const currEl = array[i];
+    if (currEl === 0 || currEl === "0") { // allow type coercion
+      const tmp = array[i];
+      for (let j = i; j < array.length - 1; j++) {
+        array[j] = array[j + 1];
+      }
+      array[array.length - 1] = tmp;
+      i--;
+    }
+    count++;
+  }
 
+  // the correctly sorted array should be returned.
+  return array;
+}
 
-//module.exports = ;
+module.exports = removeZeros;
--- test/test.js
+++ test/test.js
@@ -1,8 +1,15 @@
-//const { } = require("../app/kata");
+const removeZeros = require("../app/kata");
 const expect = require("chai").expect;
 
 describe("Tests", () => {
   it("should pass all tests", () => {
+    const input = [7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14],
+      solution = [7, 2, 3, 4, 6, 13, 78, 19, 14, 0, 0, 0, 0, 0, 0];
 
+    expect(removeZeros(input)).to.eql(solution);
+  });
+  it("should ignore non-integer values in the array", () => {
+    const input = [1, null, "0", "5", "2", 8, 0, 6, null, false],
+      solution = [1, null, "5", "2", 8, 6, null, false, "0", 0];
   });
 });
