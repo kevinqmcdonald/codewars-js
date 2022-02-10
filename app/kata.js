edeec23e75832fa2b0cb5e7bebed952c3f38e6dd
kevinqmcdonald.eth <kevinqmcdonald@gmail.com>
2022-02-09 20:25:46 -0500

    progress

--- app/kata.js
+++ app/kata.js
@@ -1,6 +1,25 @@
 // app/kata.js
+// This will multiply two numbers as strings in order
+// to circumvent the 64-bit limit
+function multiply(a, b) {
+  const aDigits = a.split("");
+  const bDigits = b.split("");
+  let product = "";
+  let carry = 0;
+  for (let i = 0; i < aDigits.length; i++) {
+    const aD
+  }
+}
 
-// Insert code here
+function factorial(n) {
+  // DP Factorial Algorithm
+  const f = Array(n);
+  f[0] = "1";
+  for (let i = 1; i <= n; i++) {
+    f[i] = multiply(`${i}`, f[i - 1]);
+  }
 
+  return f[n];
+}
 
-//module.exports = ;
+module.exports = factorial;
--- test/test.js
+++ test/test.js
@@ -1,8 +1,12 @@
-//const { } = require("../app/kata");
+const factorial = require("../app/kata");
 const expect = require("chai").expect;
 
 describe("Tests", () => {
   it("should pass all tests", () => {
-
+    expect(factorial(1)).to.equal('1');
+    expect(factorial(5)).to.equal('120');
+    expect(factorial(9)).to.equal('362880');
+    expect(factorial(15)).to.equal('1307674368000');
+    expect(factorial(25)).to.equal('15511210043330985984000000')
   });
 });
