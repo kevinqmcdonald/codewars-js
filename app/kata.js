20031fafa52ac9710086c9df34200a7d3af2d6ee
kevinqmcdonald.eth <kevinqmcdonald@gmail.com>
2022-02-11 22:00:40 -0500

    solution

--- app/kata.js
+++ app/kata.js
@@ -1,6 +1,22 @@
 // app/kata.js
+class Primes {
+  static * stream() {
+    yield 2;
+    let n = 3;
+    while(true) {
+      if(isPrime(n)) yield n;
+      n += 2;
+    }
 
-// Insert code here
+    function isPrime(n) {
+      for (let i = 3; i <= Math.sqrt(n); i += 2) {
+        if (n % i === 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+}
 
-
-//module.exports = ;
+module.exports = Primes;
--- test/test.js
+++ test/test.js
@@ -1,8 +1,15 @@
-//const { } = require("../app/kata");
+const Primes = require("../app/kata");
 const expect = require("chai").expect;
 
-describe("Tests", () => {
-  it("should pass all tests", () => {
+function verify (from_n, ...vals) {
+  const stream = Primes.stream()
+  for(let i=0; i<from_n; ++i) stream.next()
+  for(let v of vals) expect(stream.next().value).to.equal(v)
+}
 
-  });
+describe("Tests", () => {
+  it('0_10', ()=> verify(0,2,3,5,7,11,13,17,19,23,29))
+  it('10_10', ()=> verify(10,31,37,41,43,47,53,59,61,67,71))
+  it('100_10', ()=> verify(100,547,557,563,569,571,577,587,593,599,601))
+  it('1000_10', ()=> verify(1000,7927,7933,7937,7949,7951,7963,7993,8009,8011,8017))
 });
